b126d79bb2ec2a7ffdd2579716e4b762f525b16e
root <root@puppet.ebc>
2011-01-19 12:36:03 -0200

    Having an /etc/sudoers file _and_ managing it at the same time doesn't work.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,10 @@
 # sudo class
 class sudo {
   package { "sudo": ensure => present }
-  file{"/etc/sudoers": 
-    owner => "root", 
-    group => "root", 
-    mode => "440",
-    source => ["puppet:///modules/site-files/sudoers", "puppet:///modules/sudo/sudoers" ]
-  }
+  #file{"/etc/sudoers": 
+  #  owner => "root", 
+  #  group => "root", 
+  #  mode => "440",
+  #  source => ["puppet:///modules/site-files/sudoers", "puppet:///modules/sudo/sudoers" ]
+  #}
 }
